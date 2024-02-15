2d8f747ad50d2d37dce2e0ec622ee25dfbb57462
Edward Dijk <edward@edwarddijk.nl>
2024-02-15 16:18:06 +0100

    Disbled wireless networking since networkmanager is already enabled

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
 
   networking.hostName = "DijkNix"; # Define your hostname.
   # Pick only one of the below networking options.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   networking.timeServers = [ "nl.pool.ntp.org" ];
 
