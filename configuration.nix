465ae2f2dba28c46da4b00d46d96c8f2fa96c11b
Edwrad Dijk <edward@enso.nl>
2024-12-07 14:57:44 +0100

    Added Flatpak global

--- configuration.nix
+++ configuration.nix
@@ -63,6 +63,16 @@
 	enableQt5Integration = true;
 	};
 
+  services.flatpak.enable = true;
+
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
   services.chrony.enable = true;
   services.ntp.enable = false;
   services.fstrim.enable = true;
