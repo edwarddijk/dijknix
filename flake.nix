272db3059672e6742149cff54765d5a466e30a30
Edward Dijk <edward@edwardijk.nl>
2024-12-22 20:06:01 +0100

    added facter

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
     };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
@@ -12,6 +13,8 @@
 	    bequiet  = inputs.nixpkgs.lib.nixosSystem {
       	    system = "x86_64-linux";
 	    modules = [
+	    	    inputs.nixos-facter-modules.nixosModules.facter
+                    { config.facter.reportPath = ./facter.json; }
 	    	    ./hosts/bequiet/bequiet.nix
 		    ./hosts/bequiet/configuration.nix
 		    home-manager.nixosModules.home-manager
