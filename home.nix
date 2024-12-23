46ec78ae8b9048d92fa24714c1ba6bb1ba43942c
Edward Dijk <edward@edwardijk.nl>
2024-12-23 14:43:29 +0100

    typo as usual

--- home.nix
+++ home.nix
@@ -29,7 +29,7 @@
     settings = {
 
       "$terminal" = "kitty";
-      "$fileManager" = "pcmanfm";
+      "$filemanager" = "pcmanfm";
       "$menu" = "wofi --show drun";
       "$webbrowser" = "firefox";
 
