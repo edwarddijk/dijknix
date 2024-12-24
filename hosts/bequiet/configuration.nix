5b72eb01380e70ecb4bb8571c6f4b293d20cebd8
Edward Dijk <edward@edwardijk.nl>
2024-12-24 21:05:42 +0100

    alles

--- hosts/bequiet/configuration.nix
+++ hosts/bequiet/configuration.nix
@@ -5,6 +5,11 @@
 { config, lib, pkgs, ... }:
 
 {
+
+  imports = [
+    ../../modules/default.nix
+  ];
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 10;
@@ -24,77 +29,9 @@
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   networking.timeServers = [ "nl.pool.ntp.org" ];
 
-  # Set your time zone.
-  time.timeZone = "Europe/Amsterdam";
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_TIME = "nl_NL.UTF-8";
-    LC_PAPER = "nl_NL.UTF-8";
-    LC_NUMERIC = "nl_NL.UTF-8";
-    LC_MONETARY = "nl_NL.UTF-8";
-    LC_NAME = "nl_NL.UTF-8";
-    LC_ADDRESS = "nl_NL.UTF-8";
-    LC_TELEPHONE = "nl_NL.UTF-8";
-    LC_MEASUREMENT = "nl_NL.UTF-8";
-  };
-
-  console = {
-     font = "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz"; 
-     keyMap = "us";
-  };
-
-  xdg.portal = {
-  	enable = true;
-	extraPortals = with pkgs; [
-    		xdg-desktop-portal-gtk
-		xdg-desktop-portal-hyprland
-  	];
-  };
-
-  stylix = {
-    enable = true;
-    autoEnable = true;
-    image = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/ChrisTitusTech/nord-background/refs/heads/main/41.png";
-      sha256 = "2a68ce8b7ee45b73d42d47543038848622b09e18d22ab98f79ccd210445297e9";
-    };
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml"; 
-    homeManagerIntegration.autoImport = true;
-    polarity = "dark";
-    opacity.terminal = 0.8;
-    cursor.package = pkgs.bibata-cursors;
-    cursor.name = "Bibata-Modern-Ice";
-    cursor.size = 24;
-    fonts = {
-      monospace = {
-        package = pkgs.nerd-fonts.fira-mono;
-        name = "FiraMono Nerd Font";
-      };
-      sansSerif = {
-        package = pkgs.montserrat;
-        name = "Montserrat";
-      };
-      serif = {
-        package = pkgs.montserrat;
-        name = "Montserrat";
-      };
-      sizes = {
-        applications = 12;
-        terminal = 12;
-        desktop = 11;
-        popups = 12;
-      };
-    };
-
-   };
-
-  security.sudo.wheelNeedsPassword = true;
 
   services.xserver.enable = true;  
   services.greetd = {
